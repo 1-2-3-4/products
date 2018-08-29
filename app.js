2a0622f76690d8df6ff08542f18f3b2b5ddd921e
Nicole Kuprienko <nicole.kuprienko@gmail.com>
2018-08-29 19:55:45 -0400

    removes index router

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
 app.use('/products', productsRouter);
 app.use('/inventory', inventoryRouter);
 
