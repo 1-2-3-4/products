e8179dd3fd2edf43f9319d6863acea6d475ffae9
Nicole Kuprienko <nicole.kuprienko@gmail.com>
2018-08-30 09:25:59 -0400

    tabs to spaces

--- frontend/src/Dropdown.js
+++ frontend/src/Dropdown.js
@@ -1,22 +1,22 @@
 import React from 'react';
 
 class Dropdown extends React.Component {
-	render() {
-		const { label, prodOptions, handleChange, prodId } = this.props;
-		return (
-			<div className="products__dropdown">
-				<label>{ label }</label>
-				<select onChange={ (e) => handleChange(e, prodId, label) }>
-					<option value="">Select</option>
-					{
-						prodOptions.map(prodOption => {
-							return <option key={ prodOption + prodId } value={ prodOption }>{ prodOption }</option>
-						})
-					}
-				</select>
-			</div>
-		)
-	}
+    render() {
+        const { label, prodOptions, handleChange, prodId } = this.props;
+        return (
+            <div className="products__dropdown">
+                <label>{ label }</label>
+                <select onChange={ (e) => handleChange(e, prodId, label) }>
+                    <option value="">Select</option>
+                    {
+                        prodOptions.map(prodOption => {
+                            return <option key={ prodOption + prodId } value={ prodOption }>{ prodOption }</option>
+                        })
+                    }
+                </select>
+            </div>
+        )
+    }
 }
 
 export default Dropdown;
